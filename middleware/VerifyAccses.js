e99f72620e4cbbe0738175b447809b888f240181
Prins Naval Nuzeren <100395775+prinsereen@users.noreply.github.com>
2024-03-10 21:44:43 +0700

    [Feat] finalize middleware

--- middleware/VerifyAccses.js
+++ middleware/VerifyAccses.js
@@ -30,3 +30,43 @@ export const verifyAllUser = (req, res, next) => {
     });
 };
 
+
+export const verifyTeacherStudent = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+    // Check if the token is valid for student
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (!err) {
+            verifyToken(req, res, next)
+        } else {
+            // If token is not valid for student, try teacher verification
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_TEACHER, (err, decoded) => {
+                verifyTokenTeacher(req, res, next)
+            });
+        }
+    });
+};
+
+export const verifyTeacherAdmin = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+    // Check if the token is valid for student
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_TEACHER, (err, decoded) => {
+        if (!err) {
+            verifyTokenTeacher(req, res, next)
+        } else {
+            // If token is not valid for student, try teacher verification
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_ADMIN, (err, decoded) => {
+                verifyTokenAdmin(req, res, next)
+            });
+        }
+    });
+};
